dde531fa647c82aeeb5a14695c383313ede04f04
WalterPeck <clabvessels@gmail.com>
2015-02-10 20:59:47 -0500

    going at it, got most of the underbelly worked out I beleive

--- app/main/controllers/main_controller.rb
+++ app/main/controllers/main_controller.rb
@@ -1,21 +1,63 @@
 # By default Volt generates this controller for your Main component
 class MainController < Volt::ModelController
-  def index
-    # Add code for when the index view is loaded
-  end
 
+  
+  def index
+   
+  end
+ 
+ 
+  
+  
   def about
     # Add code for when the about view is loaded
   end
+	
+	
+	
+	def stop_time
+		page._new_time = 0
+	end
+	
+	  
+	def difference
+		local_store._time_diff = (Time.now - page._start_time).round
+	end
+	
+	
+ #def minute_conversion           <-----this totally killed the app
+ #	local_store.time_diff/ 60
+ #end 
+	
+	def start_time
+		page._start_time = Time.new
+		
+	end
+	
+	 
+
+	
+	
 	
 	def find_character_length
 			string = page._new_player
 			string.length 	
 	end
 	
+	def find_word_num
+	   words =	find_character_length / 5
+	end
+	
+	
 	def find_gross_wpm
-		hey = page._new_player
-		hey.length
+		if find_character_length == 1
+			start_time
+		else
+			nil
+		end
+		yolo = difference
+		return find_word_num.to_i / yolo
+		
 	end
 	
   private
